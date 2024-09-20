f8f382d0145cb16ee8e13e3df0f241003deaea33
makbuk <mazayovich.xenia@gmail.com>
2024-09-20 20:55:43 +0400

    Upgrade Homework number 5

--- src/main/kotlin/org/example/kotlincourse/Lesson5/Homework 5.kt
+++ src/main/kotlin/org/example/kotlincourse/Lesson5/Homework 5.kt
@@ -3,6 +3,7 @@ package org.example.kotlincourse.Lesson5
 // Задача 1
 //Начальную интенс-ть звука задаю переменной initialIntensity, = 100.0
 fun main() {
+
     // Задаю начальную интенсивность звука
     val initialIntensity = 100.0 // теперь это начальная интенсивность
 
@@ -19,6 +20,18 @@ fun main() {
     val intensityWithUnknownCoefficient = calculateDampedSoundIntensity(initialIntensity, unknownDampingCoefficient)
     // Печатаю результат
     println("Интенсивность звука с неизвестным коэффициентом затухания: $intensityWithUnknownCoefficient")
+    // Начинаю с определения стоимости груза
+
+    val cargoValue: Double? = null // можно тут попробовать задать значение, например, 100.0
+
+    // вызываю функцию, чтобы рассчитать полную стоимость доставки
+    val totalCost = calculateTotalShippingCost(cargoValue)
+
+    // Печатаю результат
+    println("Полная стоимость доставки: $${"%.2f".format(totalCost)}") // Форматируем для удобства
+    println(calculateTotalShippingCost(300.0))
+getPressureReading("v,dbfj")
+    getPressureReading(null)
 }
 
 // здесь функция считает интенсивность звука после затухания:
@@ -32,9 +45,9 @@ fun calculateDampedSoundIntensity(initialIntensity: Double, dampingCoefficient: 
 
 //Задача 2
 
-fun main() {
 
-    fun main() {
+
+    fun main1() {
         // Начинаю с определения стоимости груза
 
         val cargoValue: Double? = null // можно тут попробовать задать значение, например, 100.0
@@ -52,7 +65,7 @@ fun main() {
         val effectiveCargoValue = cargoValue ?: 50.0 // Если cargoValue null, использую 50.0
 
         // Шаг 2: Рассчитываю страховку, которая составляет 0,5% от стоимости груза
-        val insurance = effectiveCargoValue * 0.005 // 0.5% от стоимости
+        val insurance = effectiveCargoValue * 0.05 // 0.5% от стоимости
 
         // Шаг 3: Теперь я могу рассчитать полную стоимость доставки
         val totalShippingCost = effectiveCargoValue + insurance // Полная стоимость = стоимость груза + страховка
@@ -62,24 +75,16 @@ fun main() {
     }
 
 
-    // Задача 3
-    fun main() {
-        // задаю переменную для атмосферного давления
-        //  использую тип Double?, чтобы обозначить, что значение может быть null (отсутствовать)
-        val atmosphericPressure: Double? = getPressureReading() // Получаю показания давления
 
-        // Шаг 2: Проверяю, получили ли мы показания
-        if (atmosphericPressure == null) {
-            // Если показания отсутствуют, выводим сообщение об ошибке
-            println("Ошибка: отсутствуют показания атмосферного давления.")
-        } else {
-            // Если показания есть, выводим их на экран
-            println("Показания атмосферного давления: $atmosphericPressure гПа") //- исчисления в гигапаскалях?
-        }
-    }
+
+
+    // Задача 3
+
 
     // тут будет функция для получения показаний атмосферного давления
 
-    fun getPressureReading(): Double? {
+    fun getPressureReading(report: String?) {
+       println (report ?: throw Exception())
+
 
     }
