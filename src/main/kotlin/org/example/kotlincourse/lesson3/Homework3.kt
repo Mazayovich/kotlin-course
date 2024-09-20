1fed5a74460f2c747710b00cf1e145b2b181d0bc
makbuk <mazayovich.xenia@gmail.com>
2024-09-20 15:22:35 +0400

    Upgrade Homework number 3

--- src/main/kotlin/org/example/kotlincourse/lesson3/Homework3.kt
+++ src/main/kotlin/org/example/kotlincourse/lesson3/Homework3.kt
@@ -1,16 +1,16 @@
-package org.example.org.example.kotlincourse.lesson3
-
-val name: String = "Hackathon Survival"
-var eventDate: String = "09.11.2024"
-val location: String = "Handsome computer club Tbilisi"
-private val budget: String = "500000"
-var duration: Long = 8*60*60
-private lateinit var contactinformation: String
-var status: String = "Not Started"
-var sponsors: String = "Sponsors list"
-var internetacceslevel: Int "Don't know"
-val mood: String by lazy { "\" }
-private var info: String = "Transort information"
-lateinit var plan: String
-val task: String = "Evacuation Plan"
-
+//package org.example.org.example.kotlincourse.lesson3
+//
+//val name: String = "Hackathon Survival"
+//var eventDate: String = "09.11.2024"
+//val location: String = "Handsome computer club Tbilisi"
+//private val budget: String = "500000"
+//var duration: Long = 8*60*60
+//private lateinit var contactinformation: String
+//var status: String = "Not Started"
+//var sponsors: String = "Sponsors list"
+//var internetacceslevel: Int "Don't know"
+//val mood: String by lazy { "\" }
+//private var info: String = "Transort information"
+//lateinit var plan: String
+//val task: String = "Evacuation Plan"
+//
