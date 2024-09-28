cdfa1651be8590be79fbeda5090ef8c616b60911
makbuk <mazayovich.xenia@gmail.com>
2024-09-28 17:33:16 +0400

    Lesson 7 Practice 7

---
+++ src/main/kotlin/org/example/kotlincourse/lesson7/Practice7.kt
@@ -0,0 +1,10 @@
+package org.example.kotlincourse.lesson7
+
+fun main () {
+    for (item in 1..10) {
+        println(item)
+    }
+
+   for (item in 1..5)
+       println(item * item)
+}
---
+++ src/main/kotlin/org/example/kotlincourse/lesson7/lesson 7.kt
@@ -0,0 +1,16 @@
+package org.example.kotlincourse.lesson7
+
+fun main() {
+
+    for (i in 1..10) {
+        println(i)
+    }
+    for (i in 10 downTo 1) {
+        println(i)
+    }
+
+    for (i in 1..10 step 5) {
+        println(i)
+    }
+
+}
