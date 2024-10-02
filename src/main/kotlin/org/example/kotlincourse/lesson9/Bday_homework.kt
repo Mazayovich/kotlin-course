e427d3551fa17035748a2aa0cb7da50270d273a9
makbuk <mazayovich.xenia@gmail.com>
2024-10-02 14:26:23 +0400

    HAPPY BDAY SASHA

---
+++ src/main/kotlin/org/example/kotlincourse/lesson9/Bday_homework.kt
@@ -0,0 +1,15 @@
+package org.example.kotlincourse.lesson9
+
+fun main() {
+    val teacher = "Преподаватель"
+    val patienceLevel = Int.MAX_VALUE
+    val jokesUnderstood = 0
+    val codeKnowledgeProgress = 42
+
+    println("Поздравляю тебя, $teacher!")
+    println("Твое терпение: $patienceLevel, моё понимание шуток: $jokesUnderstood.")
+    println("Мои знания Kotlin пока на $codeKnowledgeProgress%, но это только начало!")
+
+    println("Спасибо за то, что не отправляешь нас в бесконечный цикл!")
+    println("С наилучшими пожеланиями, Ксюша! ")
+}
