ddc590d943dc90d926f5e07b683ec2f5251ac36c
makbuk <mazayovich.xenia@gmail.com>
2024-10-08 17:01:15 +0400

    HW part III. List. Peredelano

--- src/main/kotlin/org/example/kotlincourse/lesson10/Homework10.lists.kt
+++ src/main/kotlin/org/example/kotlincourse/lesson10/Homework10.lists.kt
@@ -1,119 +1,102 @@
-package org.example.kotlincourse.lesson10
-
-// Тестирование функций
 fun main() {
-    // тест]1
-    println(createEmptyIntList()) // Задание 1
-    println(createInitializedStringList()) // Задание 2
-
-    val mutableList = createMutableIntList() // Задание 3
-    println(mutableList)
-
-    addElementsToMutableList(mutableList) // Задание 4
-    println(mutableList)
-
-    val stringList = mutableListOf("Hello", "World", "Kotlin") // Пример для Задания 5
-    println(removeElementFromList(stringList, "World")) // Задание 5
-
-    printIntListElements(mutableList) // Задание 6
-
-    println(getSecondElementFromStringList(createInitializedStringList())) // Задание 7
-
-    updateElementInMutableList(mutableList, 2, 10) // Задание 8
-    println(mutableList)
-
-    val mergedList = mergeStringLists(listOf("A", "B"), listOf("C", "D")) // Задание 9
-    println(mergedList)
-
-    val (min, max) = findMinMaxInIntList(listOf(1, 2, 3, 4, 5)) // Задание 10
-    println("Min: $min, Max: $max")
-
-    val evenNumbers = filterEvenNumbers(listOf(1, 2, 3, 4, 5, 6)) // Задание 11
-    println(evenNumbers)
+    createEmptyIntList()
+    createStringList()
+    createMutableIntList()
+    addElementsToList()
+    removeElementFromList()
+    iterateList()
+    getElementByIndex()
+    updateElementByIndex()
+    combineTwoLists()
+    findMinMaxInList()
+    filterEvenNumbers()
 }
 
-
-
 // Задание 1: Создание Пустого Списка
-fun createEmptyIntList(): List<Int> {
-    return listOf()
+fun createEmptyIntList() {
+    val emptyList: List<Int> = listOf()
+    println(emptyList)
 }
 
 // Задание 2: Создание и Инициализация Списка
-fun createInitializedStringList(): List<String> {
-    return listOf("Hello", "World", "Kotlin")
+fun createStringList() {
+    val stringList = listOf("Hello", "World", "Kotlin")
+    println(stringList)
 }
 
 // Задание 3: Создание Изменяемого Списка
-fun createMutableIntList(): MutableList<Int> {
-    return mutableListOf(1, 2, 3, 4, 5)
+fun createMutableIntList() {
+    val mutableList = mutableListOf(1, 2, 3, 4, 5)
+    println(mutableList)
 }
 
 // Задание 4: Добавление Элементов в Список
-fun addElementsToMutableList(list: MutableList<Int>): MutableList<Int> {
-    list.add(6)
-    list.add(7)
-    list.add(8)
-    return list
+fun addElementsToList() {
+    val mutableList = mutableListOf(1, 2, 3, 4, 5)
+    mutableList.add(6)
+    mutableList.add(7)
+    mutableList.add(8)
+    println(mutableList)
 }
 
 // Задание 5: Удаление Элементов из Списка
-fun removeElementFromList(list: MutableList<String>, element: String): MutableList<String> {
-    list.remove(element)
-    return list
+fun removeElementFromList() {
+    val mutableStringList = mutableListOf("Hello", "World", "Kotlin")
+    mutableStringList.remove("World")
+    println(mutableStringList)
 }
 
 // Задание 6: Перебор Списка в Цикле
-fun printIntListElements(list: List<Int>) {
-    for (element in list) {
+fun iterateList() {
+    val intList = listOf(10, 20, 30, 40, 50)
+    for (element in intList) {
         println(element)
     }
 }
 
 // Задание 7: Получение Элементов Списка по Индексу
-fun getSecondElementFromStringList(list: List<String>): String? {
-    return if (list.size > 1) list[1] else null
+fun getElementByIndex() {
+    val stringList = listOf("Apple", "Banana", "Cherry")
+    println(stringList[1])  // Получение второго элемента
 }
 
 // Задание 8: Перезапись Элементов Списка по Индексу
-fun updateElementInMutableList(list: MutableList<Int>, index: Int, newValue: Int): MutableList<Int> {
-    if (index in list.indices) {
-        list[index] = newValue
-    }
-    return list
+fun updateElementByIndex() {
+    val mutableIntList = mutableListOf(10, 20, 30, 40, 50)
+    mutableIntList[2] = 100  // Замена элемента на позиции 2
+    println(mutableIntList)
 }
 
 // Задание 9: Объединение Двух Списков
-fun mergeStringLists(list1: List<String>, list2: List<String>): List<String> {
-    val mergedList = mutableListOf<String>()
-    for (item in list1) {
-        mergedList.add(item)
-    }
-    for (item in list2) {
-        mergedList.add(item)
-    }
-    return mergedList
+fun combineTwoLists() {
+    val list1 = listOf("A", "B", "C")
+    val list2 = listOf("D", "E", "F")
+    val combinedList = mutableListOf<String>()
+    for (element in list1) combinedList.add(element)
+    for (element in list2) combinedList.add(element)
+    println(combinedList)
 }
 
 // Задание 10: Нахождение Минимального/Максимального Элемента
-fun findMinMaxInIntList(list: List<Int>): Pair<Int?, Int?> {
-    var min: Int? = null
-    var max: Int? = null
-    for (element in list) {
-        if (min == null || element < min) min = element
-        if (max == null || element > max) max = element
+fun findMinMaxInList() {
+    val intList = listOf(3, 7, 2, 9, 5)
+    var min = intList[0]
+    var max = intList[0]
+    for (num in intList) {
+        if (num < min) min = num
+        if (num > max) max = num
     }
-    return Pair(min, max)
+    println("Минимум: $min, Максимум: $max")
 }
 
 // Задание 11: Фильтрация Списка
-fun filterEvenNumbers(list: List<Int>): List<Int> {
-    val evenList = mutableListOf<Int>()
-    for (element in list) {
-        if (element % 2 == 0) {
-            evenList.add(element)
+fun filterEvenNumbers() {
+    val intList = listOf(1, 2, 3, 4, 5, 6)
+    val evenNumbers = mutableListOf<Int>()
+    for (num in intList) {
+        if (num % 2 == 0) {
+            evenNumbers.add(num)
         }
     }
-    return evenList
+    println(evenNumbers)
 }
-
