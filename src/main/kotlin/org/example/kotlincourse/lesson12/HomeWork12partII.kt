aada03940f0a73a985f90351d513f63ac42c92db
makbuk <mazayovich.xenia@gmail.com>
2024-10-13 11:09:26 +0400

    HomeWork 12 pt 2

---
+++ src/main/kotlin/org/example/kotlincourse/lesson12/HomeWork12partII.kt
@@ -0,0 +1,35 @@
+package org.example.kotlincourse.lesson12
+
+// Задача 1
+fun multiplyByTwo(number: Int): Int {
+    return number * 2
+}
+
+// Задача 2
+fun isEven(number: Int): Boolean {
+    return number % 2 == 0
+}
+
+// Задача 3
+fun printNumbersUntil(n: Int) {
+    if (n < 1) return
+    for (i in 1..n) {
+        println(i)
+    }
+}
+
+// Задача 4
+fun findFirstNegative(numbers: List<Int>): Int? {
+    for (number in numbers) {
+        if (number < 0) return number
+    }
+    return null
+}
+
+// Задача 5
+fun processList(strings: List<String?>) {
+    for (string in strings) {
+        if (string == null) return
+        println(string)
+    }
+}
