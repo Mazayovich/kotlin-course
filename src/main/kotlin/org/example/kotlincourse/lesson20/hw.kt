68905781da03c439e76cfd30afeaf1c088b77f85
makbuk <mazayovich.xenia@gmail.com>
2024-11-15 16:47:18 +0400

    hw 20. i dont think i can

--- src/main/kotlin/org/example/kotlincourse/lesson20/hw.kt
+++ src/main/kotlin/org/example/kotlincourse/lesson20/hw.kt
@@ -1,22 +1,22 @@
-package org.example.kotlincourse.lesson20
-
-fun main() {
-    val fridge = object : Refrigerator(){}
-    val washingMachine = object : WashingMachine(){}
-    val smartLampp = object  : SmartLamp(){}
-    val electronicClock = object : ElectronicClock(){}
-    val robotVacuum = object : RobotVacuum(){}
-    val mechanicalClock = object : MechanicalClock(){}
-    val flashlight = object : FlashLight(){}
-    val coffeeMachine = object: CoffeeMachine(){}
-    val smartSpeaker = object : SmartSpeaker(){}
-
-
-
-    //методы
-    fridge.powerOn()
-    fridge.setTemperature()
-    washingMachine.setTimer()
-    smartLampp.emitLight()
-    smartSpeaker.programAction()
-}
+//package org.example.kotlincourse.lesson20
+//
+//fun main() {
+//    val fridge = object : Refrigerator(){}
+//    val washingMachine = object : WashingMachine(){}
+//    val smartLampp = object  : SmartLamp(){}
+//    val electronicClock = object : ElectronicClock(){}
+//    val robotVacuum = object : RobotVacuum(){}
+//    val mechanicalClock = object : MechanicalClock(){}
+//    val flashlight = object : FlashLight(){}
+//    val coffeeMachine = object: CoffeeMachine(){}
+//    val smartSpeaker = object : SmartSpeaker(){}
+//
+//
+//
+//    //методы
+//    fridge.powerOn()
+//    fridge.setTemperature()
+//    washingMachine.setTimer()
+//    smartLampp.emitLight()
+//    smartSpeaker.programAction()
+//}
